8d1d262b86739bdbafbb081713782401d6ad1622
Daniel Bugl <me@omnidan.net>
2016-02-28 23:53:29 +0100

    init project :sparkles:

---
+++ README.md
@@ -0,0 +1,54 @@
+# karma
+
+![https://i.imgur.com/9p9uWAu.png](https://i.imgur.com/9p9uWAu.png)
+
+_manage activities with a points/karma system_
+
+
+## Setup
+
+Clone this repository:
+
+```
+git clone https://github.com/omnidan/karma
+cd karma
+```
+
+Install dependencies (in the `karma` directory):
+
+```
+npm install
+```
+
+
+## Running
+
+```
+node index.js
+```
+
+
+## Commands
+
+ * `help` - view all commands with infos
+ * `help [command]` - view info about a certain command
+
+ * `def <name> <points>` - define an activity
+ * `def <name> <points> -n` - define an activity with negative points
+
+ * `list` - list all activities
+ * `history` - list a history of all activities
+
+ * `<activity> [times]` or `do <activity> [times]` - do a certain activity certain times, e.g. `coding 60` could be "coding for 60 minutes"
+
+
+## Resetting db
+
+You can reset the db by running:
+
+```
+npm run reset
+```
+
+**WARNING:** This will remove all your data.
+
---
+++ index.js
@@ -0,0 +1,129 @@
+var vorpal = require('vorpal')()
+var colors = require('colors')
+var fs = require('fs')
+
+var storage = {}
+try {
+  var rawStorage = JSON.parse(fs.readFileSync('db.json'))
+  storage.points = rawStorage.points
+  storage.history = rawStorage.history
+  storage.acts = {}
+  Object.keys(rawStorage.acts).forEach(act => define(act, rawStorage.acts[act]))
+} catch (e) {
+  storage = {
+    points: 0,
+    acts: {},
+    history: []
+  }
+}
+
+function persist () {
+  fs.writeFileSync('db.json', JSON.stringify(storage))
+}
+
+function points (act) {
+  return storage.acts[act]
+}
+
+function redraw () {
+  vorpal
+    .delimiter('karma (' + storage.points + ') $')
+}
+
+function addHistory (name, points, times) {
+  storage.history.push({ name, points, times, date: Date.now() })
+  persist()
+}
+
+function makeAction (name, diff) {
+  return function action (args, callback) {
+    var times = args.times || 1
+    storage.points += diff * times
+    addHistory(name, diff, times)
+    persist()
+    redraw()
+    callback()
+  }
+}
+
+function define (name, points) {
+  var isNew = !storage.acts.hasOwnProperty(name)
+
+  storage.acts[name] = points
+  persist()
+
+  if (!isNew) vorpal.find('do ' + name).remove()
+  vorpal
+    .command('do ' + name + ' [times]')
+    .description('Execute if you\'ve done `' + name + '` (you can also just type `' + name + '`)')
+    .alias(name)
+    .action(makeAction(name, points))
+}
+
+vorpal
+  .command('def <name> <points>')
+  .description('Defines an activity and points for it')
+  .alias('define')
+  .alias('d')
+  .option('-n, --negative', 'subtract points instead of adding')
+  .action(function (args, callback) {
+    var points = args.options.negative ? -args.points : args.points
+    define(args.name, points)
+    this.log('successfully defined activity `' + args.name + '` (' + points + ' points)')
+    callback()
+  })
+
+vorpal
+  .command('list')
+  .description('List activites and points')
+  .alias('l')
+  .action(function (args, callback) {
+    this.log()
+    Object.keys(storage.acts).forEach(act => this.log('  ' + act + ': ' + points(act)))
+    this.log()
+    callback()
+  })
+
+vorpal
+  .command('history')
+  .description('Show history of activities and points')
+  .alias('h')
+  .action(function (args, callback) {
+    this.log()
+    var highestNum = { points: 0, times: 0, name: 0 }
+    storage.history.forEach(h => {
+      if (Math.abs(h.points) > highestNum.points) highestNum.points = Math.abs(h.points)
+      if (Math.abs(h.times) > highestNum.times) highestNum.times = Math.abs(h.times)
+      if (h.name.length > highestNum.name) highestNum.name = h.name.length
+    })
+
+    var maxLength = {
+      points: String(highestNum.points).length,
+      times: String(highestNum.times).length,
+      name: highestNum.name
+    }
+    storage.history.forEach(h => {
+      var prefix = h.points > 0 ? '+' : ''
+      var curLength = {
+	points: String(Math.abs(h.points)).length,
+	times: String(Math.abs(h.times)).length,
+	name: h.name.length
+      }
+      prefix = ' '.repeat(maxLength.points - curLength.points) + prefix
+      var timesPrefix = ' '.repeat(maxLength.times - curLength.times)
+      var msg = '  ' + prefix + h.points + ' * ' + timesPrefix + h.times + ' (' + h.name + ')'
+      var datePrefix = '  ' + ' '.repeat(maxLength.name - curLength.name)
+      this.log((h.points > 0 ? msg.green : msg.red) + datePrefix + (new Date(h.date)).toString())
+    })
+    this.log('---' + '-'.repeat(maxLength.points) + '---' + '-'.repeat(maxLength.times) + '--' + '-'.repeat(maxLength.name) + '-')
+    var endPrefix = storage.points > 0 ? '+' : ''
+    var endMsg = '  ' + endPrefix + storage.points
+    this.log(storage.points > 0 ? endMsg.green : endMsg.red)
+    this.log()
+    callback()
+  })
+
+redraw()
+
+vorpal
+  .show()
